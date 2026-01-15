4415f005e66cafaa25e800c2ab1003a519107984
Daniel Sharp <dannys4@mit.edu>
2026-01-15 11:50:26 -0500

    Add AB example file

---
+++ examples/example_aristoff_bangerth.py
@@ -0,0 +1,98 @@
+# %%
+import torch
+from nak_torch.tools import animate_trajectories_box
+from nak_torch.functions import aristoff_bangerth as ab, build_aristoff_bangerth
+from nak_torch.algorithms import msip_greedy
+from matplotlib import ticker
+import matplotlib.pyplot as plt
+torch.set_default_device("cpu")
+
+# # %%
+# N, N_obs = 32, 13
+# solve_args = ab.build_forward_solver_args(N, N_obs, torch.float32)
+# thetas = torch.as_tensor(ab.theta_true, device=solve_args[0].device).repeat(11,1)
+
+# # %%
+# hi_res_obs = ab.build_forward_solver_args(N, 128, torch.float32)[0]
+
+# # %%
+# out = ab.forward_solver(thetas, N, *solve_args)
+# obs = (hi_res_obs @ out.T).reshape(128, 128, -1).permute(2, 0, 1)
+
+# %%
+log_p = build_aristoff_bangerth(use_compiled=True, dtype=torch.float64)
+log_th = torch.randn(3, 64, requires_grad=True, dtype=torch.float64)
+test_out = log_p(log_th)
+test_eval = torch.autograd.grad(test_out.sum(), log_th)
+
+# %%
+n_particles = 16
+init_particles = 2 * torch.randn((n_particles, 64),
+                                 dtype=torch.float64, device='cpu')  # Sample from prior
+trajectories, _ = msip_greedy(
+    log_p,
+    n_particles=n_particles,
+    n_steps=50,  # "epochs" (passes over all particles)
+    dim=64,
+    bounds=(-8, 8),   # [a,b]^d
+    gradient_informed=True,
+    projection=True,
+    lr=0.8,
+    # noise=0.05,          # currently unused
+    init_particles=init_particles,
+    kernel_bandwidth=0.6,
+    bandwidth_factor=0.5,
+    inner_tol=1e-6,      # equilibrium tolerance for a particle
+    max_inner_steps=5,  # max inner iterations per particle
+    seed=0,
+    # diag_infl=1e-5,
+    device="cpu"
+)
+
+# %%
+N_solver, N_viz = 32, 128
+last_state = trajectories[-1]
+H_vis = ab.build_forward_solver_args(N_solver, N_viz)[0]
+
+# %%
+pts = trajectories[-1]
+fig = plt.figure(figsize=(10, 6), layout='constrained')
+side_len = 4
+gs = fig.add_gridspec(side_len, side_len + 2)
+plt_kwargs = {'vmin': pts.min(), 'vmax': pts.max(), 'extent': (0, 8, 0, 8)}
+
+for i in range(side_len):
+    for j in range(side_len):
+        ax = fig.add_subplot(gs[i, j])
+        # ax.set_axis_off()
+        ax.set_aspect('equal')
+        t = ax.matshow(pts[i*4 + j].reshape(8, 8), **plt_kwargs)
+        # ax.vlines(jnp.arange(1,8), -0.1, 8.1, color='w', lw=0.75)
+        # ax.hlines(jnp.arange(1,8), -0.1, 8.1, color='w', lw=0.75)
+        ax.minorticks_on()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.xaxis.set_minor_locator(ticker.MultipleLocator())
+        ax.yaxis.set_minor_locator(ticker.MultipleLocator())
+        ax.grid(which="both", linewidth=1.5, color="w")
+        ax.tick_params(which="minor", length=0)
+ax_cb = fig.add_subplot(gs[:-2, -2:])
+ax_cb.set_title(r"Scale of $\log\theta$", y=0.6)
+cax_cb = ax_cb.inset_axes((0.1, 0.45, 0.8, 0.1))
+ax_cb.axis('off')
+fig.colorbar(t, cax=cax_cb, orientation='horizontal')
+ax_true = fig.add_subplot(gs[-2:, -2:])
+ax_true.set_aspect('equal')
+ax_true.matshow(ab.theta_true.log().reshape(8, 8), **plt_kwargs)
+ax_true.minorticks_on()
+ax_true.set_xticks([])
+ax_true.set_yticks([])
+ax_true.xaxis.set_minor_locator(ticker.MultipleLocator())
+ax_true.yaxis.set_minor_locator(ticker.MultipleLocator())
+ax_true.set_title(r"True $\theta$")
+ax_true.grid(which="both", linewidth=1.5, color="w")
+ax_true.tick_params(which="minor", length=0)
+plt.show()
+
+# %%
+plt.imshow(torch.mean(torch.tensor(trajectories[1000]), 0).reshape(8, 8))
