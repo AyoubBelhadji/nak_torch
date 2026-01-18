dc51c6b694957d3116952c136077f3af32d3b5b7
Daniel Sharp <dannys4@mit.edu>
2026-01-18 18:40:59 -0500

    add opt arg to gaussian kernel mat

--- src/nak_torch/algorithms/kernel.py
+++ src/nak_torch/algorithms/kernel.py
@@ -1,7 +1,9 @@
 import torch
+from typing import Optional
 
-def gaussian_kernel_matrix(particles_leaf, sigma2):
-    diff = particles_leaf.unsqueeze(1) - \
-                particles_leaf.unsqueeze(0)  # (N, N, d)
+def gaussian_kernel_matrix(particles_leaf: torch.Tensor, sigma2: float, p_leaf2: Optional[torch.Tensor] = None):
+    if p_leaf2 is None:
+        p_leaf2 = particles_leaf
+    diff = particles_leaf.unsqueeze(1) - p_leaf2.unsqueeze(0)  # (N, N, d)
 
     return torch.exp(- (diff ** 2).sum(dim=-1) / sigma2)
