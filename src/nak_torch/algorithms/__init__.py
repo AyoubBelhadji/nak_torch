cf7e131b4524ed6e645e078ceb26c2ddb1ec8753
Daniel Sharp <dannys4@mit.edu>
2026-01-22 12:17:58 -0500

    Create MSIP directory

--- src/nak_torch/algorithms/__init__.py
+++ src/nak_torch/algorithms/__init__.py
@@ -8,10 +8,7 @@
 # 05/12/2025
 
 from .eks import eks
-from .msip import msip
-from .msip_ni import msip_ni
-from .msip_greedy import msip_greedy
-from .msip_geom_greedy import msip_geom_greedy
+from .msip import msip, msip_ni, msip_greedy, msip_geom_greedy
 from .svgd import svgd
 from .grad_aldi import grad_aldi
 from .gradfree_aldi import gradfree_aldi
---
+++ src/nak_torch/algorithms/msip/__init__.py
@@ -0,0 +1,11 @@
+from .msip import msip
+from .msip_greedy import msip_greedy
+from .msip_ni import msip_ni
+from .msip_geom_greedy import msip_geom_greedy
+
+__all__ = [
+    "msip",
+    "msip_greedy",
+    "msip_ni",
+    "msip_geom_greedy",
+]
