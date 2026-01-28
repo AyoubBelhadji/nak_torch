6ac7f5d5489461f0c4b458091e2ddfdd6498a933
Daniel Sharp <dannys4@mit.edu>
2026-01-27 21:46:54 -0500

    Add more examples

--- slurm/problems/__init__.py
+++ slurm/problems/__init__.py
@@ -1,10 +1,14 @@
 from dataclasses import dataclass
 from typing import Callable, Optional
+from functools import partial
 
 import torch
 from nak_torch.tools import GaussianModel
 from nak_torch.tools.types import BatchPtType, BatchLogDensity
-from . import joker, aristoff_bangerth
+from . import joker, aristoff_bangerth, kfrflow_examples
+
+def gaussian_sampler(d: int, rng: torch.Generator, n_samples: int):
+    return torch.normal(0., 1., size=(n_samples, d), generator=rng)
 
 @dataclass
 class Problem:
@@ -20,3 +24,27 @@ def joker_logpdf():
     rng.manual_seed(0)
     samples = joker.sample(rng, 10000)
     return Problem(torch.compile(joker.logpdf), samples, joker.prior_sample)
+
+def butterfly_logpdf():
+    model = GaussianModel(
+        kfrflow_examples.butterfly_model,
+        1 / kfrflow_examples.butterfly_like_var,
+        1., kfrflow_examples.butterfly_obs, is_vectorized=True
+    )
+    return Problem(model, None, partial(gaussian_sampler, 2))
+
+def spaceships_logpdf():
+    model = GaussianModel(
+            kfrflow_examples.spaceships_model,
+        1 / kfrflow_examples.spaceships_like_var,
+        1., kfrflow_examples.spaceships_obs, is_vectorized=True
+    )
+    return Problem(model, None, partial(gaussian_sampler, 2))
+
+def doughnut_logpdf():
+    model = GaussianModel(
+            kfrflow_examples.doughnut_model,
+        1 / kfrflow_examples.doughnut_like_var,
+        1., kfrflow_examples.doughnut_obs, is_vectorized=True
+    )
+    return Problem(model, None, partial(gaussian_sampler, 2))
---
+++ slurm/problems/kfrflow_examples.py
@@ -0,0 +1,27 @@
+import torch
+from torch import Tensor
+from dataclasses import dataclass
+from functools import partial
+from typing import Callable, Any
+from nak_torch import GaussianModel
+
+doughnut_model = torch.compile(lambda pt: torch.linalg.norm(pt, -1))
+doughnut_obs = 2.
+doughnut_like_var = 0.25**2
+
+butterfly_model = torch.compile(lambda pt: torch.sin(pt[...,1]) + torch.cos(pt[...,0]))
+butterfly_obs = -1.
+butterfly_like_var = 0.6**2
+
+spaceships_model = torch.compile(lambda pt: torch.sin(pt[...,0] * pt[...,1]) + torch.cos(pt[...,0] * pt[...,1]))
+spaceships_obs = -1.
+spaceships_like_var = 0.5**2
+
+@torch.compile
+def funnel_logpdf(pt: torch.Tensor, var1: float = 9.):
+    d = pt.shape[-1]
+    x0 = pt[...,0]
+    prior_term = torch.sum(pt**2, -1) + torch.log(2*torch.pi*torch.ones(()))
+    term_0 = x0**2 / var1 + torch.log(var1*torch.ones(()))
+    rest_terms = torch.sum(pt[...,1:]**2,-1) / torch.exp(x0)  + (d-1)*x0
+    return -0.5 * (prior_term + term_0 + rest_terms)
