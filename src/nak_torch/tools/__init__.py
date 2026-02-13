16803c732a2c82d1fb50968dbca987566cda4ecd
Daniel Sharp <dannys4@mit.edu>
2026-02-12 20:07:53 -0500

    Fix tools

--- src/nak_torch/tools/__init__.py
+++ src/nak_torch/tools/__init__.py
@@ -3,14 +3,12 @@
 # 05/12/2025
 
 
-from .viz_tools.twodims_viz_tools import animate_trajectories_box
 from . import kernel, types, quadrature, adaptive_step
 from .average import recursive_weighted_average_alpha_v
 from .torchify import differentiable_density_factory
 from .types import GaussianModel
 
 __all__ = [
-    "animate_trajectories_box",
     "kernel",
     "types",
     "recursive_weighted_average_alpha_v",
