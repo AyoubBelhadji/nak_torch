88f713512d8e76f1defdb526ea8d46ccbe2e65fb
AyoubBelhadji <ayoub.belhadji@gmail.com>
2026-03-27 10:33:23 -0400

    Implementation of Gauss Seidel MSIP + example where msip is compared to msip_gs

--- src/nak_torch/algorithms/msip/__init__.py
+++ src/nak_torch/algorithms/msip/__init__.py
@@ -1,4 +1,5 @@
 from .msip import msip
+from .msip_gs import msip_gs
 from .msip_greedy import msip_greedy
 from .msip_ni import msip_ni
 from .msip_geom_greedy import msip_geom_greedy
@@ -7,6 +8,7 @@ from .estimators import MSIPEstimator, MSIPQuadGradientFree, MSIPFredholm, MSIPQ
 
 __all__ = [
     "msip",
+    "msip_gs",
     "msip_greedy",
     "msip_ni",
     "msip_geom_greedy",
